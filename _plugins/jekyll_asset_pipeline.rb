e0df472724c3516023e0df102e601914db83bfba
Thijs van den Anker <thijs@capitalv.nl>
2013-10-11 23:12:19 +0200

    pipeline voor css

---
+++ _plugins/jekyll_asset_pipeline.rb
@@ -0,0 +1,39 @@
+gem "jekyll-asset-pipeline", "=0.1.5"
+require "jekyll_asset_pipeline"
+
+module JekyllAssetPipeline
+  class SassConverter < JekyllAssetPipeline::Converter
+    require 'sass'
+
+    def self.filetype
+      '.scss'
+    end
+
+    def convert
+      return Sass::Engine.new(@content, syntax: :scss).render
+    end
+  end
+  class CssCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.css'
+    end
+
+    def compress
+      return YUI::CssCompressor.new.compress(@content)
+    end
+  end
+
+  class JavaScriptCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.js'
+    end
+
+    def compress
+      return YUI::JavaScriptCompressor.new(munge: true).compress(@content)
+    end
+  end
+end
